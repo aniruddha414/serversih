275f9282ec00d0001607a910c09130b490d1e37b
aniruddha414 <akt4567@gmail.com>
2020-01-17 16:53:18 +0530

    addded application id gen and fetch form

--- routes/form.js
+++ routes/form.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let router = express.Router();
 let storeForm = require('../components/form/storeForm.js');
+let getApplicationID = require('../components/form/getNewApplID.js')
+let fetchForm = require('../components/form/fetchForm.js');
 let collectionNames = {};
 let getCollectionNames = function (names) {
 	collectionNames = names;
@@ -10,11 +12,20 @@ router.get('/form', function(req, res, next) {
 	res.send('form api');
 });
 
+router.get('/getNewID',function (req, res, next) {
+	getApplicationID.getNewID(collectionNames,req,res);
+});
+
 router.post('/storeForm',function (req,res) {
 	console.log('Storing form to mongo db');
 	console.log(req.body);
-  storeForm.storeForm(collectionNames,req,res);
+	storeForm.storeForm(collectionNames,req,res);
   // res.send("1")
+});
+
+router.post('/fetchForm',function(req,res) {
+	console.log("fetching form router");
+	fetchForm.fetchForm(collectionNames,req,res);
 });
 
 module.exports = router;
